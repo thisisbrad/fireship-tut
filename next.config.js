cac4470752e4b5649f94adf482e1c05a51ebb671
Brad Beltowski <bradbeltowskigmail.com>
2024-10-09 17:03:44 -0400

    Images work with Next now

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.scdn.co",
-        port: "",
-        pathname: "/image/**",
-      },
-      {
-        protocol: "https",
-        hostname: "platform-lookaside.fbsbx.com",
-        port: "",
-        pathname: "/platform/profilepic/**",
-      },
-    ],
+    domains: ["scontent-iad3-1.xx.fbcdn.net"],
   },
 };
 
