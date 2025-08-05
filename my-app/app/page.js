8e8a286d46660098d5c846253bd7816ac5320788
Brad Beltowski <bradbeltowskigmail.com>
2025-08-05 18:12:14 -0400

    Added todo

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import VideoHero from "./components/VideoHero";
-
+// generate metadata
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
